db533fc5979f2677ec95d545bf917b753d15e946
Thiago Farias <84402079+thygo10@users.noreply.github.com>
2023-02-15 10:28:02 -0300

    up

---
+++ script.js
@@ -0,0 +1,11 @@
+
+  const cookieImg = document.querySelector(".cookie");
+  const cardDiv = document.querySelector(".card");
+  const luckCardDiv = document.querySelector(".luck-card");
+
+  cookieImg.addEventListener("click", () => {
+    console.log("Clicou na imagem do cookie");
+    cardDiv.classList.toggle("hide");
+    luckCardDiv.classList.toggle("hide");
+  });
+
